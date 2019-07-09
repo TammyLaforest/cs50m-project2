a1c92e1132ab76efacdca19a144a3bd43ce37746
TammyLaforest <TammyLaforest@gmail.com>
2019-07-09 11:47:10 -0400

    Hello, World

---
+++ App.js
@@ -0,0 +1,11 @@
+// import { } from 'expo';
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello, World!</Text>
+    </View>
+  )
+}
